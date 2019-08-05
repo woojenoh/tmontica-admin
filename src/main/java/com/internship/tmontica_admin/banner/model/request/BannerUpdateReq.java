f975964a22d5d78d57c62977e9e76777f74f548b
정미수 <jungmisu@tmon.co.kr>
2019-08-05 10:23:02 +0900

    [banner] 배너 수정, 삭제 구현

--- src/main/java/com/internship/tmontica_admin/banner/model/request/BannerUpdateReq.java
+++ src/main/java/com/internship/tmontica_admin/banner/model/request/BannerUpdateReq.java
@@ -1,4 +1,40 @@
 package com.internship.tmontica_admin.banner.model.request;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
 public class BannerUpdateReq {
+
+    @NotEmpty(message = "usePage는 필수입니다.")
+    private String usePage;
+
+    private boolean usable = true;
+
+    @NotNull
+    private MultipartFile imgFile;
+
+    @NotEmpty(message = "link는 필수입니다.")
+    private String link;
+
+    @NotNull(message = "시작일은 필수입니다.")
+    @DateTimeFormat(pattern = "yyyy.MM.dd HH:mm:ss")
+    private Date start_date;
+
+    @NotNull(message = "종료일은 필수입니다.")
+    @DateTimeFormat(pattern = "yyyy.MM.dd HH:mm:ss")
+    private Date end_date;
+
+    @NotNull(message = "순서는 필수입니다.")
+    @Min(1) @Max(10)
+    private int number;
 }
