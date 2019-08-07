09151aa07d3710cdd9ae8185bd848d957fd61fb9
노우제 <woojenoh@gmail.com>
2019-08-07 21:37:59 +0900

    [Stat] 메뉴 차트 API 연결

--- front-end/src/pages/Statistics/index.tsx
+++ front-end/src/pages/Statistics/index.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
+import axios from "axios";
 import { Bar, Doughnut } from "react-chartjs-2";
 import Header from "../../components/Header";
 import Nav from "../../components/Nav";
+import * as menuTypes from "../../types/menu";
+import * as statTypes from "../../types/statistics";
+import { API_URL } from "../../api/common";
+import { withJWT } from "../../utils";
 import "./styles.scss";
-
-export interface IStatisticsProps {}
-
-export interface IStatisticsState {}
 
 const colors = [
   "#ff6b6b",
@@ -33,29 +34,6 @@ const hoverColors = [
   "#228be6"
 ];
 
-const data1 = {
-  datasets: [
-    {
-      label: "아메리카노",
-      backgroundColor: colors[0],
-      hoverBackgroundColor: hoverColors[0],
-      data: [5520]
-    },
-    {
-      label: "카페라떼",
-      backgroundColor: colors[1],
-      hoverBackgroundColor: hoverColors[1],
-      data: [2310]
-    },
-    {
-      label: "바닐라라떼",
-      backgroundColor: colors[2],
-      hoverBackgroundColor: hoverColors[2],
-      data: [3420]
-    }
-  ]
-};
-
 const data2 = {
   labels: ["10대", "20대", "30대"],
   datasets: [
@@ -78,8 +56,194 @@ const data3 = {
   ]
 };
 
+export interface IStatisticsProps {}
+
+export interface IStatisticsState {
+  menus: menuTypes.IMenu[] | null;
+  chartMenus: statTypes.IStatMenu[] | null;
+  menuId: string;
+  menuStartDate: string;
+  menuEndDate: string;
+  // 날짜 적용 당시의 날짜를 기억하기 위한 객체
+  capturedMenuDate: {
+    menuStartDate: string;
+    menuEndDate: string;
+  };
+}
+
+export interface IInputState {
+  menuId: string;
+  menuStartDate: string;
+  menuEndDate: string;
+}
+
 class Statistics extends React.Component<IStatisticsProps, IStatisticsState> {
+  state = {
+    menus: null,
+    chartMenus: null,
+    menuId: "",
+    menuStartDate: "",
+    menuEndDate: "",
+    capturedMenuDate: {
+      menuStartDate: "",
+      menuEndDate: ""
+    }
+  } as IStatisticsState;
+
+  componentDidMount() {
+    axios.get(`${API_URL}/menus`, withJWT({ params: { size: 100 } })).then(res => {
+      this.setState({
+        menus: res.data.menus
+      });
+    });
+  }
+
+  handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value
+    } as { [K in keyof IInputState]: IInputState[K] });
+  };
+
+  handleSelectChange = (e: React.FormEvent<HTMLSelectElement>) => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value
+    } as { [K in keyof IInputState]: IInputState[K] });
+  };
+
+  // 메뉴 차트에 추가
+  handleMenuSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const { menuId, chartMenus, capturedMenuDate } = this.state;
+    e.preventDefault();
+    if (menuId) {
+      axios
+        .post(
+          `${API_URL}/statistic/menu`,
+          {
+            menuIds: [Number(menuId)],
+            startDate: capturedMenuDate.menuStartDate,
+            endDate: capturedMenuDate.menuEndDate
+          },
+          withJWT()
+        )
+        .then(res => {
+          // null 체크
+          if (chartMenus) {
+            // 메뉴가 10개 이하인지 체크
+            if (chartMenus.length <= 10) {
+              // 차트에 같은 메뉴가 있는지
+              if (chartMenus.map(m => m.menuId).indexOf(Number(menuId)) < 0) {
+                this.setState({
+                  chartMenus: chartMenus.concat(res.data.dataList)
+                });
+              } else {
+                alert("이미 차트에 존재하는 메뉴입니다.");
+              }
+            } else {
+              alert("통계 메뉴는 10개를 초과할 수 없습니다.");
+            }
+          } else {
+            this.setState({
+              chartMenus: res.data.dataList
+            });
+          }
+        })
+        .catch(err => alert(err.response.data.message));
+    } else {
+      alert("추가할 메뉴를 선택하세요.");
+    }
+  };
+
+  // 차트에 들어간 메뉴 삭제
+  handleMenuRemove = (chartIndex: number) => {
+    const { chartMenus } = this.state;
+    if (chartMenus) {
+      // 길이가 있을 경우에만
+      if (chartMenus.length !== 0) {
+        this.setState({
+          chartMenus: chartMenus.filter((m, index) => {
+            return index !== chartIndex;
+          })
+        });
+      } else {
+        alert("메뉴를 추가하세요.");
+      }
+    } else {
+      alert("메뉴를 추가하세요.");
+    }
+  };
+
+  // 메뉴 차트에 기간 적용
+  handleMenuPeriodSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const { chartMenus, menuStartDate, menuEndDate } = this.state;
+    e.preventDefault();
+    if (chartMenus) {
+      axios
+        .post(
+          `${API_URL}/statistic/menu`,
+          {
+            menuIds: chartMenus.map(m => m.menuId),
+            startDate: menuStartDate,
+            endDate: menuEndDate
+          },
+          withJWT()
+        )
+        .then(res =>
+          this.setState({
+            chartMenus: res.data.dataList,
+            // 적용된 날짜를 기억해둔다.
+            capturedMenuDate: {
+              menuStartDate: menuStartDate,
+              menuEndDate: menuEndDate
+            }
+          })
+        )
+        .catch(err => alert(err.response.data.message));
+    } else {
+      alert("메뉴를 먼저 추가하세요.");
+    }
+  };
+
+  handleMenuPeriodAllSubmit = () => {
+    const { chartMenus } = this.state;
+    if (chartMenus) {
+      axios
+        .post(
+          `${API_URL}/statistic/menu`,
+          {
+            menuIds: chartMenus.map(m => m.menuId),
+            startDate: "",
+            endDate: ""
+          },
+          withJWT()
+        )
+        .then(res =>
+          this.setState({
+            chartMenus: res.data.dataList,
+            menuStartDate: "",
+            menuEndDate: "",
+            capturedMenuDate: {
+              menuStartDate: "",
+              menuEndDate: ""
+            }
+          })
+        )
+        .catch(err => alert(err.response.data.message));
+    } else {
+      alert("메뉴를 먼저 추가하세요.");
+    }
+  };
+
   render() {
+    const { menus, chartMenus, menuId, menuStartDate, menuEndDate } = this.state;
+    const {
+      handleInputChange,
+      handleSelectChange,
+      handleMenuSubmit,
+      handleMenuRemove,
+      handleMenuPeriodSubmit,
+      handleMenuPeriodAllSubmit
+    } = this;
+
     return (
       <>
         <Header title="통계" />
@@ -89,39 +253,89 @@ class Statistics extends React.Component<IStatisticsProps, IStatisticsState> {
             <section className="statistics p-4">
               <div className="menu-chart">
                 <div className="chart-form-container">
-                  <h1 className="mb-4 text-center">메뉴별 판매량</h1>
-                  <form className="chart-add-form">
-                    <select className="form-control">
-                      <option value="아메리카노">아메리카노</option>
-                      <option value="카페라떼">카페라떼</option>
+                  <h1 className="mb-4 text-center">메뉴별 매출액</h1>
+                  <form className="chart-add-form" onSubmit={e => handleMenuSubmit(e)}>
+                    <select
+                      className="form-control"
+                      name="menuId"
+                      value={menuId}
+                      onChange={e => handleSelectChange(e)}
+                      required
+                    >
+                      <option value="">선택</option>
+                      {menus &&
+                        menus.map(m => {
+                          return (
+                            <option key={m.id} value={m.id}>
+                              {m.nameKo}
+                            </option>
+                          );
+                        })}
                     </select>
                     <input className="btn btn-primary w-100 mt-2 mb-4" type="submit" value="추가" />
                   </form>
-                  <form className="chart-period-form">
-                    <input className="form-control mb-2" type="date" />
-                    <input className="form-control mb-2" type="date" />
+                  <form className="chart-period-form" onSubmit={e => handleMenuPeriodSubmit(e)}>
+                    <input
+                      className="form-control mb-2"
+                      name="menuStartDate"
+                      type="date"
+                      value={menuStartDate}
+                      onChange={e => handleInputChange(e)}
+                      required
+                    />
+                    <input
+                      className="form-control mb-2"
+                      name="menuEndDate"
+                      type="date"
+                      value={menuEndDate}
+                      onChange={e => handleInputChange(e)}
+                      required
+                    />
                     <input className="btn btn-primary w-100 mb-4" type="submit" value="적용" />
-                    <button className="btn btn-outline-primary w-100">기간 전체 보기</button>
                   </form>
+                  <button
+                    className="btn btn-outline-primary w-100"
+                    onClick={() => handleMenuPeriodAllSubmit()}
+                  >
+                    기간 전체 보기
+                  </button>
                 </div>
                 <div className="chart-wrapper pl-4">
                   <Bar
-                    data={data1}
+                    data={
+                      chartMenus
+                        ? {
+                            datasets: chartMenus.map((m: statTypes.IStatMenu, index: number) => {
+                              return {
+                                label: m.menuName,
+                                backgroundColor: colors[index],
+                                hoverBackgroundColor: hoverColors[index],
+                                data: [m.totalPrice]
+                              };
+                            })
+                          }
+                        : {}
+                    }
                     options={{
                       responsive: true,
                       maintainAspectRatio: false,
                       tooltips: {
-                        enabled: false
+                        callbacks: {
+                          title: function() {}
+                        }
                       }
                     }}
-                    getElementAtEvent={elem => console.log(elem)}
+                    getElementAtEvent={elem =>
+                      // 선택된 바가 있을 경우에만 해당 바 삭제.
+                      elem.length ? handleMenuRemove(elem[0]._datasetIndex) : {}
+                    }
                   />
                 </div>
               </div>
 
               <div className="age-chart">
                 <div className="chart-form-container">
-                  <h1 className="mb-4 text-center">연령별 분포량</h1>
+                  <h1 className="mb-4 text-center">연령별 매출액</h1>
                   <form className="chart-add-form">
                     <select className="form-control">
                       <option value="10대">10대</option>
@@ -153,7 +367,7 @@ class Statistics extends React.Component<IStatisticsProps, IStatisticsState> {
 
               <div className="device-chart">
                 <div className="chart-form-container">
-                  <h1 className="mb-4 text-center">기기별 접속량</h1>
+                  <h1 className="mb-4 text-center">기기별 접속자수</h1>
                   <form className="chart-add-form">
                     <select className="form-control">
                       <option value="10대">10대</option>
---
+++ front-end/src/types/statistics.ts
@@ -0,0 +1,12 @@
+export interface IStatMenu {
+  menuId: number;
+  menuName: string;
+  totalPrice: number;
+}
+
+export interface IStatMenuData {
+  label: string;
+  backgroundColor: string;
+  hoverBackgroundColor: string;
+  data: number[];
+}
